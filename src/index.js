d635cfb7518e02e498bdf21430ebb2893ab253d2
Yura <yura5742248@gmail.com>
2021-08-23 22:05:32 +0300

    init: init array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    return array.reduce(function (p, v) {
+        return (p < v ? p : v);
+    });
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return array.reduce(function (p, v) {
+        return (p > v ? p : v);
+    });
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    for (let i = 0; i < array.length; i++) {
+        let sum = 0
+        for (let j = 0; j < array.length; j++) {
+            sum += array[j]
+        }
+        let avr = sum / array.length
+        return avr;
+    }
+
 }
+
